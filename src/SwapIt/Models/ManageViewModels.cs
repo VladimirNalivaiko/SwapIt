248872f77a06376a16b77d3a2dece6cd6aa2e2fd
vladimir <nalivaikovova@gmail.com>
2016-10-18 19:30:35 +0300

    Update users profile

--- src/SwapIt/Models/ManageViewModels.cs
+++ src/SwapIt/Models/ManageViewModels.cs
@@ -7,6 +7,7 @@ namespace SwapIt.Models
 {
     public class IndexViewModel
     {
+        public IEnumerable<SwapIt.Models.AdModel> UseAdModels { get; set; }
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
